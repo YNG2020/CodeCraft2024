30d8729d404059a02cf62c5698c4fad28bfc1204
wimlim <wimlim33@gmail.com>
2024-03-08 02:19:53 +0800

    Add basic shipDecision

--- src/global_vars.hpp
+++ src/global_vars.hpp
@@ -16,7 +16,9 @@ Berth berth[berth_num];
 Boat boat[boat_num];
 
 int money, boat_capacity, id;
-char ch[N][N];
-int gds[N][N];
+char map[N][N];
+int goods[N][N];
+int dx[4] = {0, 0, -1, 1};
+int dy[4] = {1, -1, 0, 0};
 
 #endif // GLOBAL_VARS_HPP
--- src/main.cpp
+++ src/main.cpp
@@ -5,16 +5,23 @@
 #include "berth.hpp"
 #include "boat.hpp"
 #include "global_vars.hpp"
+#include "decisionmaker.hpp"
 
 using namespace std;
 
 void Init() {
-    for (int i = 1; i <= n; i++)
-        cin >> (ch[i] + 1);
+    for (int i = 1; i <= n; i++) {
+        cin >> (map[i] + 1);
+    }
     for (int i = 0; i < berth_num; i++) {
         int id;
         cin >> id;
         cin >> berth[id].x >> berth[id].y >> berth[id].transport_time >> berth[id].loading_speed;
+    }
+    for (int i = 0; i < boat_num; i++) {
+        boat[i].num = 0;
+        boat[i].status = 1;
+        boat[i].pos = -1;
     }
     cin >> boat_capacity;
     string okk;
@@ -30,10 +37,10 @@ int Input() {
     for (int i = 1; i <= num; i++) {
         int x, y, val;
         cin >> x >> y >> val;
+        goods[x][y] = val;
     }
     for (int i = 0; i < robot_num; i++) {
-        int sts;
-        cin >> robot[i].goods >> robot[i].x >> robot[i].y >> sts;
+        cin >> robot[i].goods >> robot[i].x >> robot[i].y >> robot[i].status;
     }
     for (int i = 0; i < 5; i++)
         cin >> boat[i].status >> boat[i].pos;
@@ -43,12 +50,12 @@ int Input() {
 }
 
 int main() {
-    srand(time(nullptr)); // Seed for random number generation
+    srand((unsigned int)time(nullptr)); // Seed for random number generation
+    DecisionMaker decisionMaker;
     Init();
     for (int zhen = 1; zhen <= 15000; zhen++) {
         int id = Input();
-        for (int i = 0; i < robot_num; i++)
-            cout << "move " << i << " " << rand() % 4 << endl;
+        decisionMaker.makeDecision();
         cout << "OK" << endl;
         cout.flush();
     }
