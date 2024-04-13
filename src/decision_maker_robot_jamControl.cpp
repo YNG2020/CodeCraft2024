d57e4809620889f21172eaa0c63a37d8f9f240d4
wimlim <wimlim33@gmail.com>
2024-04-13 13:49:02 +0800

    删除注释

--- src/decision_maker_robot_jamControl.cpp
+++ src/decision_maker_robot_jamControl.cpp
@@ -7,10 +7,6 @@
 // 更新robot的避让优先级
 void DecisionMaker::setPriority()
 {
-    // for (int i = 0; i < robotNum; ++i) {
-    //     priorityFactor[i][0] = i;   // 第一列放编号
-    //     priorityFactor[i][1] = robot[i].goodsVal;   // 第二列放货物的价值
-    // }
     for (int i = 0; i < robotNum; ++i)
     {
         // robot[i].avoidPriority = i;
